df6334039519f6178cd2481fc30b2d6132b59450
fiatjaf <fiatjaf@gmail.com>
2016-10-09 22:05:01 -0400

    I like things more now, although they still do not work.

--- Main.elm
+++ Main.elm
@@ -10,6 +10,7 @@ import Json.Decode as J exposing ((:=))
 import Platform.Cmd as Cmd
 import Array exposing (Array)
 import Array.Extra
+import Dict exposing (Dict)
 import Navigation exposing (Location)
 import Process
 import String
@@ -31,49 +32,71 @@ main =
 
 init : Location -> (Model, Cmd Msg)
 init _ =
-    ( { baseItem | name = "Summalist" }
+    ( Model { baseItem | name = "Summalist" }
     , Cmd.none
     )
 
 type alias Model =
-    { name : String
-    , children : Children
-    , id : List Int
-    , nextChildId : Int
+    { item : Item
     }
 
-type Children = Items (Array Model)
+type alias Item =
+    { id : Id
+    , name : String
+    , children : Children
+    , sorted : Array Id
+    , nextChildSeq : Int
+    }
 
-baseItem : Model
-baseItem = Model "" (Items Array.empty) [] 0
+type alias Id = List Int
+
+type Children = Items (Dict Id Item)
+
+baseItem : Item
+baseItem = Item [1] "" (Items Dict.empty) Array.empty 1
 
 
 -- UPDATE
 
 type Msg
-    = UpdateItem String
-    | AddChild
-    | RemoveChild Int
-    | ChildMsg Int Msg
+    = UpdateItem Id String
+    | AddItem Id
+    | RemoveItem Id
     | Focus String
     | NoOp
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-        UpdateItem value ->
-            { model | name = value } ! []
-        AddChild ->
+        UpdateItem id value ->
             let
-                (Items children) = model.children
-                childId = log "adding with id" <| model.nextChildId :: model.id
+                u item = { item | name = value }
             in
-                { model | children = Items
-                    <| Array.push
-                        { baseItem | id = childId }
-                        children
-                , nextChildId = model.nextChildId + 1
-                } !
+                { model | item = updateDeep id u model.item } ! []
+        RemoveItem id ->
+            { model | item = removeDeep id model.item } ! []
+        AddItem id ->
+            let
+                childId = case getDeep id model.item of
+                    Nothing -> []
+                    Just immediateparent ->
+                        immediateparent.nextChildSeq :: id |> List.reverse
+                x = log "childId" childId
+                u item =
+                    let
+                        (Items children) = item.children
+                    in
+                        { item
+                            | children = Items
+                                <| Dict.insert
+                                    childId
+                                    { baseItem | id = childId }
+                                    children
+                            , sorted = Array.push childId item.sorted
+                            , nextChildSeq = item.nextChildSeq + 1
+                        }
+            in
+                { model | item = updateDeep id u model.item } !
                 [ Process.sleep 100
                     |> Task.perform
                         (always NoOp)
@@ -83,33 +106,18 @@ update msg model =
                             <| log "will focus" <| makeId childId
                         )
                 ]
-        RemoveChild i ->
-            let (Items children) = model.children
-            in { model | children = Items <| Array.Extra.removeAt i children } ! []
-        ChildMsg i itemMsg ->
-            handleChildMsg i itemMsg model
         Focus id ->
             let x = log "focusing" id
             in model ! [ Dom.focus id |> Task.perform (always NoOp) (always NoOp) ]
         NoOp -> (model, Cmd.none)
 
-
-handleChildMsg : Int -> Msg -> Model -> (Model, Cmd Msg)
-handleChildMsg i msg model =
-    let
-        u item = fst <| update msg item
-        (Items children) = model.children
-    in
-        { model | children = Items <| Array.Extra.update i u children
-        } ! []
-
-keyHandler : (Msg -> Msg) -> J.Decoder Msg
-keyHandler wrapper =
+keyHandler : Id -> J.Decoder Msg
+keyHandler id =
     J.customDecoder ("keyCode" := J.int) 
         ( \code ->
             Ok <| case code of
-                13 -> wrapper AddChild -- enter
-                _ -> wrapper NoOp
+                13 -> AddItem id -- enter
+                _ -> NoOp
         )
 
 
@@ -117,45 +125,101 @@ keyHandler wrapper =
 
 view : Model -> Html Msg
 view model =
-    let
-        wrapper = identity
-    in
-        node "html" []
-            [ node "link" [ rel "stylesheet", href "style.css" ] []
-            , mainItemView wrapper model
-            ]
+    node "html" []
+        [ node "link" [ rel "stylesheet", href "style.css" ] []
+        , mainItemView model.item
+        ]
 
-mainItemView : (Msg -> Msg) -> Model -> Html Msg
-mainItemView wrapper item =
-    let
-        (Items children) = item.children
-    in
-        node "main" []
-            [ input
-                [ value item.name
-                , id <| (++) "edit-" <| makeId item.id
-                , onInput (\v -> wrapper <| UpdateItem v)
-                , on
-                    "keydown"
-                    (keyHandler wrapper)
-                ]
-                [ text item.name ]
-            , ul []
-                (Array.toList <| Array.indexedMap (childView wrapper) children)
-            , a [ href "#", onClick <| wrapper AddChild ] [ text "+" ]
+mainItemView : Item -> Html Msg
+mainItemView item =
+    node "main" []
+        [ input
+            [ value item.name
+            , id <| (++) "edit-" <| makeId item.id
+            , onInput (\v -> UpdateItem item.id v)
+            , on
+                "keydown"
+                (keyHandler item.id)
             ]
+            [ text item.name ]
+        , ul []
+            (Array.toList <| Array.map (childView << getChild item.children) item.sorted)
+        , a [ onClick <| AddItem item.id ] [ text "+" ]
+        ]
 
-childView : (Msg -> Msg) -> Int -> Model -> Html Msg
-childView parentWrapper i child =
-    let
-        wrapper = ChildMsg i << parentWrapper
-    in
-        li [ class "item" ]
-            [ a [ href "#", onClick (parentWrapper <| RemoveChild i) ] [ text "×" ]
-            , lazy2 mainItemView wrapper child
-            ]
+childView : Item -> Html Msg
+childView child =
+    li [ class "item" ]
+        [ a [ onClick (RemoveItem child.id) ] [ text "×" ]
+        , lazy mainItemView child
+        ]
 
 -- HELPERS
 
-makeId : List Int -> String
+getDeep : Id -> Item -> Maybe Item
+getDeep id ancestor =
+    let
+        (Items children) = ancestor.children
+        x = log ("searching for " ++ (makeId id) ++ " in") ancestor
+        keyLength = List.length id - List.length ancestor.id
+    in
+        if keyLength == 0 then
+            Just ancestor
+        else if keyLength >= 1 then
+            case Dict.get id children of
+                Nothing -> Nothing
+                Just child -> getDeep id child
+        else
+            Nothing
+
+updateDeep : Id -> (Item -> Item) -> Item -> Item
+updateDeep id func ancestor =
+    let
+        (Items children) = ancestor.children
+        x = log ("updating " ++ (makeId id) ++ " in") ancestor
+        keyLength = List.length id - List.length ancestor.id
+        k = log "keyl" keyLength
+    in
+        if keyLength == 0 then
+            func ancestor
+        else if keyLength >= 1 then
+            case Dict.get id children of
+                Nothing -> ancestor
+                Just child ->
+                    { ancestor
+                        | children = Items
+                            <| Dict.insert id (updateDeep id func child) children
+                    }
+        else
+            ancestor
+
+removeDeep : Id -> Item -> Item
+removeDeep id ancestor =
+    let
+        (Items children) = ancestor.children
+        x = log ("removeing in " ++ (makeId id) ++ " in") ancestor
+        keyLength = List.length id - List.length ancestor.id
+        k = log "keyl" keyLength
+    in
+        if keyLength == 0 then
+            { ancestor
+                | children = Items <| Dict.remove id children
+            }
+        else if keyLength >= 1 then
+            case Dict.get id children of
+                Nothing -> ancestor
+                Just child -> removeDeep id child
+        else
+            ancestor
+
+makeId : Id -> String
 makeId = String.join "-" << List.map toString
+
+getChild : Children -> Id -> Item
+getChild pchildren id =
+    let
+        (Items uchildren) = pchildren
+    in
+        case Dict.get id uchildren of
+            Nothing -> { baseItem | id = id }
+            Just item -> item
